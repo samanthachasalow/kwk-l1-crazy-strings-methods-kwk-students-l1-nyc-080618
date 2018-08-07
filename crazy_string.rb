9208b36de283f864e58562181d595655e888c788
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-07 14:34:25 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,9 @@
 # write your method here
 
-def crazy_strings
-puts "#{Hello}","#{Friends}" 
+def crazy_strings(first_string, second_string)
+first_string = first_string.reverse.upcase 
+second_string = second_string.swapcase
+return '#{first_string} #{second_string}
 end
 
 crazy_strings
